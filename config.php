558aa09dd696952a940e7a2b1dca9fd277f288ac
Joseph <rajakumaransundararajan@gmail.com>
2023-09-06 19:19:10 +0530

    First commit

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+
+// Keep this file very secured. You may keep it in the folders below Document Root.
+
+ 
+	// You will get above 4 variales on settings Page.	
+$username =  '7595201'; // Username
+$password =  'MgUFY5em'; // Password
+$secret =    '6KeetYvu2TSdsegD'; // API key
+$mercid = '21720'; //Merchant ID
+?>
